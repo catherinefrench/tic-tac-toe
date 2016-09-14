fb739f29f5d3d65ec4da3dba452be55c3cfe1f1a
Catherine French <catherine.french@me.com>
2016-09-14 16:12:40 -0400

    Adds conditional statement within click function to evaluate whether a value is assigned to the clicked square.

--- ttt.js
+++ ttt.js
@@ -1,5 +1,5 @@
  //Ensure DOM is ready first
-'use strict'
+'use strict';
 
  $(function() {
    var turn = 0; //sets turn variable to 0 within function so it's accessible throughout scope of function
@@ -7,16 +7,15 @@
    $('.square').on('click', function() { //binds event handler to anything with class of square
      //event handler runs when any element with class of square is clicked
     //  alert("A square was clicked");
+    if ( $(this).text() === '') { //conditional checks whether square has a value assigned
+      if (turn % 2 === 0) { // conditional to decide if X or O displayed depending on whether turn object is divisible by 2 or not
+        $(this).text('X');
+      } else {
+        $(this).text('O');
+      }
 
-    if (turn % 2 === 0) { // conditional to decide if X or O displayed depending on whether turn object is divisible by 2 or not
-      $(this).text('X');
-    } else {
-      $(this).text('O');
+      turn++; // adds 1 per turn to counter
     }
-
-      turn++;
-
     //this = the DOM element on which the event occured (value always changing), the $ makes it a jQuery object
    });
-
  });
