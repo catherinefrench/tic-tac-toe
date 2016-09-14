b71e1e7834333d1ee73ec2989663fd573dd951e6
Catherine French <catherine.french@me.com>
2016-09-14 15:33:29 -0400

    Writes JS function to bind event handler to every event of squares. For every click event, display text X.

--- ttt.js
+++ ttt.js
@@ -1,8 +1,10 @@
  //Ensure DOM is ready first
  $(function() {
-   $('.square').on('click', function() {
+   $('.square').on('click', function() { //binds event handler to anything with class of square
+     //event handler runs when any element with class of square is clicked
     //  alert("A square was clicked");
     $(this).text('X');
+    //this = the DOM element on which the event occured (value always changing), the $ makes it a jQuery object
    });
 
  });
