cc3ec6b8cf19fbfd7b9c1e298515ab0a08d09637
Catherine French <catherine.french@me.com>
2016-09-14 15:38:33 -0400

    Adds conditional that displays either X or O depending on whether turn variable is % by 2.

--- ttt.js
+++ ttt.js
@@ -1,11 +1,18 @@
  //Ensure DOM is ready first
 
  $(function() {
-   var turn = 0; //sets turn variable to 0 within function so it's accessible throughout scope of function 
+   var turn = 0; //sets turn variable to 0 within function so it's accessible throughout scope of function
 
    $('.square').on('click', function() { //binds event handler to anything with class of square
      //event handler runs when any element with class of square is clicked
     //  alert("A square was clicked");
+
+    if (turn % 2 === 0) { // conditional to decide if X or O displayed depending on whether turn object is divisible by 2 or not
+      $(this).text('X');
+    } else {
+      $(this).text('0');
+    }
+
     $(this).text('X');
     //this = the DOM element on which the event occured (value always changing), the $ makes it a jQuery object
    });
