99164924ab2c5b27aa4e386a576cbff0f17bcb69
Catherine French <catherine.french@me.com>
2016-09-14 15:36:21 -0400

    Sets var for turn to 0 within ttt.js function.

--- ttt.js
+++ ttt.js
@@ -1,5 +1,8 @@
  //Ensure DOM is ready first
+
  $(function() {
+   var turn = 0; //sets turn variable to 0 within function so it's accessible throughout scope of function 
+
    $('.square').on('click', function() { //binds event handler to anything with class of square
      //event handler runs when any element with class of square is clicked
     //  alert("A square was clicked");
